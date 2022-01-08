7ace177c4147910c1c27c4c04e9d48bd5e56f1c5
Ashish Kumar <get.mail.ashish@gmail.com>
2022-01-08 20:51:31 +0530

    second commit

--- script.js
+++ script.js
@@ -5,30 +5,47 @@ element.addEventListener("submit", (event) => {
 
 const list = document.querySelector("#userInput-1");
 const btn1 = document.querySelector(".btn-1");
+const searchElement = document.querySelector("#userInput-2");
+const btn2 = document.querySelector(".btn-2");
 const displayInput = document.querySelector(".Input");
-const displaymaxOutput = document.querySelector(".output-max");
-const displayminOutput = document.querySelector(".output-min");
+const displayOutput = document.querySelector(".Output");
 
-const maxminFind = (arr) => {
-  let min = arr[0];
-  let max = arr[0];
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] < min) {
-      min = arr[i];
-    } else if (arr[i] > max) {
-      max = arr[i];
+const searchUsingBS = (arr, searchElement) => {
+  let start = 0;
+  let end = arr.length - 1;
+  while (start <= end) {
+    let middle = start + Math.floor((end - start) / 2);
+    console.log(middle);
+    if (arr[middle] < searchElement) {
+      start = middle + 1;
+    } else if (arr[middle] > searchElement) {
+      end = middle - 1;
+    } else {
+      return " Found";
     }
   }
-  return { max, min };
+  return " Not Found";
 };
 
+let data = null;
 btn1.addEventListener("click", (e) => {
-  let data = null;
+  e.preventDefault();
   const val = list.value.split(",").map((i) => Number(i.trim()));
   val.includes(NaN) ? alert("Wrong Input") : (data = val);
   displayInput.textContent = data;
-  const { max, min } = maxminFind(data);
-  displaymaxOutput.textContent = max;
-  displayminOutput.textContent = min;
-  list.value = "";
+});
+
+btn2.addEventListener("click", (e) => {
+  let val = null;
+  let count = 0;
+  e.preventDefault();
+  if (!isNaN(Number(searchElement.value))) {
+    console.log(data);
+    console.log(Number(searchElement.value));
+    val = searchUsingBS(data, Number(searchElement.value));
+  } else {
+    count += 1;
+    alert("Something went wrong! Try Again");
+  }
+  displayOutput.textContent = count >= 1 ? "No output Found" : val;
 });
